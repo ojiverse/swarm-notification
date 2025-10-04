febc51f4f7b267c8d909cd81d0c2bd7d12a5ed61
Shinra Yamakawa <13755428+OldBigBuddha@users.noreply.github.com>
2025-10-04 17:53:43 +0900

    chore: remove secret manager from terraform definition (#12)
    SSIA

--- infra/modules/secret_manager/main.tf
+++ infra/modules/secret_manager/main.tf
@@ -10,25 +10,11 @@ resource "google_secret_manager_secret" "secrets" {
   }
 }
 
-# Secret versions for dummy values (managed by Terraform)
-resource "google_secret_manager_secret_version" "dummy_secret_versions" {
-  for_each = { for k, v in var.secrets : k => v if v.use_dummy }
-
-  secret      = google_secret_manager_secret.secrets[each.key].id
-  secret_data = each.value.dummy_value
-  
-  lifecycle {
-    ignore_changes = [secret_data]
-  }
-}
-
-# Secret versions for real values (not managed by Terraform after initial creation)
-resource "google_secret_manager_secret_version" "real_secret_versions" {
-  for_each = { for k, v in var.secrets : k => v if !v.use_dummy }
-
-  secret      = google_secret_manager_secret.secrets[each.key].id
-  secret_data = each.value.value
-}
+# Note: Secret versions are NOT managed by Terraform.
+# Secret containers are created, but values must be set manually via:
+# gcloud secrets versions add SECRET_NAME --data-file=-
+#
+# This prevents Terraform from creating/destroying secret versions.
 
 resource "google_secret_manager_secret_iam_member" "cloud_run_access" {
   for_each = var.secrets
